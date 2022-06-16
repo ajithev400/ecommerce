3affa1c99b2301fc16338f4d0a7c236d75b4fc25
ajithev400 <84377528+ajithev400@users.noreply.github.com>
2022-06-16 08:55:09 +0530

    Update settings.py

--- thedoo/settings.py
+++ thedoo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','thedoo-cart.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','thedoo-cart.herokuapp.com','*']
 
 
 # Application definition
