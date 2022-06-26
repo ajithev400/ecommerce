c46cf113b8cd6ed2a29ff9313c35ba87fa129168
ajithev400 <ajithev2202gmail.com>
2022-06-26 17:51:46 +0530

    settings allowed host changed

--- thedoo/settings.py
+++ thedoo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','thedoo-cart.herokuapp.com','*']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','thedoo-cart.herokuapp.com','thedoo.ml','3.84.17.137']
 
 
 # Application definition
